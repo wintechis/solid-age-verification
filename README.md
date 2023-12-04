316ecdb4dc41031c9d4ee3a0d3eec3b3523f518e
Monir <monir@monirs-air.localdomain>
2023-12-04 01:06:24 +0100

    init project

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # solid-age-verification
+Welcome to Solid Age Verification, an innovative shop application designed to ensure secure and seamless age verification using SOLID pods. This platform seeks user consent to access information stored in their personal SOLID pods. Leveraging the Authority PODs, This app securely store and validate user ages based on their unique WebIDs, ensuring a trusted and privacy-centric age verification process. With SOLID technology, it will prioritize user control over their data while guaranteeing accurate and reliable age verification for a safe shopping experience.
+
+## Mission
+At Solid Age Verification, This app mission is to revolutionize age verification in online shopping by harnessing the power of SOLID pods. To enabling a seamless, user-centric experience that prioritizes privacy and control over personal data. Through this innovative approach, we aim to establish a trusted and secure framework using Authority PODs to accurately verify user ages, ensuring compliance with age-restricted purchases while upholding the principles of transparency, security, and user empowerment within the SOLID ecosystem. Our goal is to redefine age verification, offering a solution that respects individual privacy while fostering a safe and inclusive online shopping environment.
+
+## Roadmap
+    ### Phase 1: Foundation and Authentication
+        Setup and Authentication Flow
+        Implement the initial Express server with SOLID authentication using solid-auth-client.
+        Create routes for user login, authentication callbacks, and profile retrieval from SOLID pods.
+    ### Phase 2: User Consent and Information Access
+        User Consent Management
+        Design and implement a user interface for requesting access to SOLID pod information, ensuring transparency about data usage and storage.
+        Develop logic to handle user consent and grant permissions to access necessary data for age verification.
+    ### Phase 3: Authority POD Integration for Age Verification
+        Authority PODs Integration
+        Research and identify Authority PODs or storage mechanisms for securely storing verified user age information based on WebIDs.
+        Establish connections and protocols to write and validate age data against Authority PODs.
+    ### Phase 4: Age Verification Process
+        Age Verification Workflow
+        Define the age verification process within the application's flow, integrating SOLID pod access and Authority POD validation.
+        Develop mechanisms to verify and authenticate user age information securely.
+
+## How to start the apps
+
+Install the dependencie.
+
+```bash
+npm install
+```
+
+Start all the frontends of the shop app:
+
+```bash
+npm start
+```
+
---
+++ src/index.js
@@ -0,0 +1,218 @@
+
+const fs = require("fs");
+const log = require("loglevel");
+const N3 = require('n3');
+log.setLevel("TRACE");
+
+// The only import we need from the Node AuthN library is the Session class.
+const { Session } = require("@inrupt/solid-client-authn-node");
+//const { client } = require("@inrupt/solid-client");
+// import { addStringNoLocale, addUrl, createSolidDataset, createThing, getPodUrlAll, getSolidDataset, saveSolidDatasetAt, setThing } from "@inrupt/solid-client";
+const clientApplicationName = "SOLID age verification";
+
+const express = require("express");
+const { resourceUsage } = require("process");
+
+const app = express();
+const PORT = 3001;
+
+// The HTML we return on all requests (that contains template placeholders that
+// we replace as appropriate).
+const indexHtml = "./src/index.html";
+
+const markerOidcIssuer = "{{oidcIssuer}}";
+const markerLoginStatus = "{{labelLoginStatus}}";
+const markerLogoutStatus = "{{labelLogoutStatus}}";
+const markerResourceToRead = "{{resourceToRead}}";
+const markerResourceValueRetrieved = "{{resourceValueRetrieved}}";
+
+const oidcIssuer = "https://monir.solidcommunity.net/";
+
+const enterResourceUriMessage =
+  "...but enter any resource URI to attempt to read it...";
+
+// We expect these values to be overwritten as the users interacts!
+const loggedOutStatus = "";
+let resourceToRead = enterResourceUriMessage;
+let resourceValueRetrieved = "...not read yet...";
+let loginStatus = "Not logged in yet.";
+
+// Initialised when the server comes up and is running...
+let session;
+
+app.get("/", (_req, res) => {
+  loginStatus = session.info.isLoggedIn
+    ? `Logged in as [${session.info.webId}]`
+    : `Not logged in`;
+
+  sendHtmlResponse(res);
+});
+
+app.get("/login", async (req, res, next) => {
+  const { oidcIssuer } = req.query;
+
+  if (session.info.isLoggedIn) {
+    loginStatus = `Already logged in with WebID [${session.info.webId}].`;
+    log.info(loginStatus);
+    sendHtmlResponse(res);
+  } else if (oidcIssuer) {
+    try {
+      await session.login({
+        redirectUrl: "http://localhost:3001/redirect",
+        oidcIssuer,
+        clientName: clientApplicationName,
+        handleRedirect: (data) => {
+          res.writeHead(302, {
+            location: data,
+          });
+          res.end();
+        },
+      });
+
+      loginStatus = `Login called, expecting redirect function to redirect the user's browser now...`;
+      log.info(loginStatus);
+    } catch (error) {
+      loginStatus = `Login attempt failed: [${error}]`;
+      log.error(loginStatus);
+      sendHtmlResponse(res);
+    }
+  } else {
+    next(
+      new Error(
+        "No OIDC issuer provided to login API (expected 'oidcIssuer' query parameter)!",
+      ),
+    );
+  }
+});
+
+app.get("/redirect", async (req, res) => {
+  try {
+    log.debug(`Got redirect: [${getRequestFullUrl(req)}]`);
+    await session
+      .handleIncomingRedirect(getRequestFullUrl(req))
+      .then((info) => {
+        log.info(`Got INFO: [${JSON.stringify(info, null, 2)}]`);
+        if (info === undefined) {
+          loginStatus = `Received another redirect, but we are already handling a previous redirect request - so ignoring this one!`;
+          sendHtmlResponse(res);
+        } else if (info.isLoggedIn) {
+          // This is deliberately making a big assumption about the structure of
+          // the logged-in WebID, but if the WebID does have a different
+          // structure, which is perfectly valid of course, then this code
+          // shouldn't alter it, and the user is free to enter whatever resource
+          // URI they actually want anyway, so it's just a convenience in the
+          // 'common' case.
+          resourceToRead = info.webId.replace("/profile/card#me", "/private/wishlist");
+
+          loginStatus = `Successfully logged in with WebID: [${info.webId}].`;
+          resourceValueRetrieved = `...logged in successfully - now fetch a resource.`;
+          sendHtmlResponse(res);
+        } else {
+          loginStatus = `Got redirect, but not logged in.`;
+          sendHtmlResponse(res);
+        }
+      });
+  } catch (error) {
+    log.error(`Error processing redirect: [${error}]`);
+
+    loginStatus = `Redirected, but failed to handle this as an OAuth2 redirect: [${error}]`;
+    sendHtmlResponse(res);
+  }
+});
+
+app.get("/fetch", async (req, res) => {
+  const resourceToFetch = req.query.resource;
+
+  // Only attempt to fetch if the resource is not our message to enter a URI!
+  if (resourceToFetch === enterResourceUriMessage) {
+    resourceValueRetrieved =
+      "Please enter a resource URI in the edit box above!";
+  } else {
+    // Update our state with whatever was in the query param.
+    resourceToRead = resourceToFetch;
+
+    try {
+      // Validate our input as a URL first.
+      new URL(resourceToFetch);
+
+      try {
+        const response = await session.fetch(resourceToFetch);
+       // session.fetch(resourceToFetch,)
+        
+       
+        resourceValueRetrieved = await response.text();
+       /* const parser = new N3.Parser();
+        const store = parser.parse(resourceValueRetrieved);
+        log.info(store);
+        */
+        log.info(`Fetch response: [${resourceValueRetrieved}]`);
+      } catch (error) {
+        resourceValueRetrieved = `Failed to fetch from resource [${resourceToFetch}]: ${error}`;
+        log.error(resourceValueRetrieved);
+      }
+    } catch (error) {
+      resourceValueRetrieved = `Resource to fetch must be a valid URL - got an error parsing [${resourceToFetch}]: ${error}`;
+      log.error(resourceValueRetrieved);
+    }
+  }
+
+  sendHtmlResponse(res);
+});
+
+app.get("/logout", async (_req, res, next) => {
+  try {
+    await session.logout();
+    resourceToRead = enterResourceUriMessage;
+    resourceValueRetrieved =
+      "...nothing read yet - click 'Read Pod Resource' button above...";
+
+    loginStatus = `Logged out successfully.`;
+    sendHtmlResponse(res);
+  } catch (error) {
+    log.error(`Logout processing failed: [${error}]`);
+    loginStatus = `Logout processing failed: [${error}]`;
+    sendHtmlResponse(res);
+  }
+});
+
+app.listen(PORT, async () => {
+  session = new Session();
+
+  log.info(
+    `[${clientApplicationName}] successfully initialized - listening at: [http://localhost:${PORT}]`,
+  );
+});
+
+function sendHtmlResponse(response) {
+  response
+    .writeHead(200, { "Content-Type": "text/html" })
+    .write(statusIndexHtml());
+  response.end();
+}
+
+function getRequestFullUrl(request) {
+  return `${request.protocol}://${request.get("host")}${request.originalUrl}`;
+}
+
+function getRequestQueryParam(request, param) {
+  return `${request.protocol}://${request.get("host")}${request.originalUrl}`;
+}
+
+function statusIndexHtml() {
+  return fs
+    .readFileSync(indexHtml, "utf8")
+    .split(markerOidcIssuer)
+    .join(oidcIssuer)
+
+    .split(markerLoginStatus)
+    .join(loginStatus)
+
+    .split(markerLogoutStatus)
+    .join(loggedOutStatus)
+
+    .split(markerResourceToRead)
+    .join(resourceToRead)
+
+    .split(markerResourceValueRetrieved)
+    .join(resourceValueRetrieved);
+}
